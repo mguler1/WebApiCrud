1ecdb7661d398d629bbd11844151d9a57160ac7a
= <=>
2021-03-22 22:59:48 +0300

    Proje dosyası ekle.

---
+++ WebApiCrud/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCrud.DaL.Context;
+using WebApiCrud.DaL.Entities;
+
+namespace WebApiCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult  Get()
+        {
+            using var context = new WebApiCrudContext();
+            return Ok( context.Categories.ToList());
+            
+        }
+    }
+}
---
+++ WebApiCrud/DaL/Context/WebApiCrudContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCrud.DaL.Entities;
+
+namespace WebApiCrud.DaL.Context
+{
+    public class WebApiCrudContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = MEHDI; database = Api2021; integrated security = true; ");
+           
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
